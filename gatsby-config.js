dc2dda67cfa0a79f1fb9ac54cd0738042d31afd6
llupRisnglll <luisedward.miranda@gmail.com>
2022-08-27 05:12:03 +0800

    chore: update the meta title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Sarmati - Mir4 Calculator`,
+    description: `Crafting cost calculator for Mir4`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
