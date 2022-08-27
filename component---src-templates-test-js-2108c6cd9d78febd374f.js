47be88b08a8b0342e65e8be01b817b8e383e88ed
llupRisnglll <luisedward.miranda@gmail.com>
2022-08-28 05:17:39 +0800

    Updates

---
+++ component---src-templates-test-js-2108c6cd9d78febd374f.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[99],{5682:function(e,t,n){n.r(t),n.d(t,{Head:function(){return s}});var a=n(7294),r=n(1597),l=n(5592),u=n(9357),s=function(){return a.createElement(u.Z,{title:"Using DSG"})};t.default=function(){return a.createElement(l.Z,null,a.createElement("h1",null,"Hello this is a test page"),a.createElement(r.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-templates-test-js-2108c6cd9d78febd374f.js.map
